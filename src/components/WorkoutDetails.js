c98836d9a3165fee8bd2639b4c3f78b9bf4ed7d6
Irin-27 <singh.nitin2714@gmail.com>
2025-02-15 10:48:08 +0530

    updated

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 import { useAuthContext } from '../hooks/useAuthContext'
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect }from 'react'
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 import { useAuthContext } from "../hooks/useAuthContext"
