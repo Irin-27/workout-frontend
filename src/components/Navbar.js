2adbe939c847e98481e758775e0005da5236d137
Irin-27 <singh.nitin2714@gmail.com>
2025-04-16 14:26:10 +0530

    Add media queries

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
 import { useAuthContext } from '../hooks/useAuthContext'
+
 const Navbar = () => {
   const { logout } = useLogout()
-  const {user} = useAuthContext()
+  const { user } = useAuthContext()
+  const [menuOpen, setMenuOpen] = useState(false)
+
   const handleClick = () => {
     logout()
   }
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <header>
       <div className="container">
         <Link to="/">
-         <h1>FitOrbit .</h1>   
+          <h1>FitOrbit</h1>   
         </Link>
-        <nav>
+        
+        {/* Mobile menu toggle button - visible only on small screens */}
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <div className={`hamburger ${menuOpen ? 'open' : ''}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        
+        <nav className={menuOpen ? 'active' : ''}>
           {user && (
             <div>
-            <span>
-              {user.email}
-            </span>
-            <button onClick={handleClick}>Log out</button>
-          </div>
+              <span className="user-email">
+                {user.email}
+              </span>
+              <button onClick={handleClick} className="logout-btn">Log out</button>
+            </div>
           )}
 
           {!user && (
             <div>
-            <Link to="/login">Log in</Link>
-            <Link to="/signup">Sign up</Link>
-          </div>
+              <Link to="/login" onClick={() => setMenuOpen(false)}>Log in</Link>
+              <Link to="/signup" onClick={() => setMenuOpen(false)}>Sign up</Link>
+            </div>
           )}
         </nav>
       </div>
