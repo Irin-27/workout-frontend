9a60f43395dbf7837339b7c0bec1ef310849faea
Irin-27 <singh.nitin2714@gmail.com>
2025-04-17 15:05:12 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useAuthContext } from './hooks/useAuthContext';
-
+import { BrowserRouter, Routes, Route , Navigate} from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 // pages & components
-import Dashboard from '../src/components/Dashboard';
-import Home from '../src/pages/Home';
-import Signup from '../src/pages/Signup';
-import Login from '../src/pages/Login';
-import UserProfile from '../src/pages/UserProfile';
-import Navbar from '../src/components/Navbar';
-import Footer from '../src/components/Footer';
-
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 function App() {
-  const { user } = useAuthContext();
-  
+  const {user}=useAuthContext()
   return (
     <div className="App">
       <BrowserRouter>
@@ -22,30 +17,23 @@ function App() {
           <Routes>
             <Route 
               path="/" 
-              element={user ? <Dashboard /> : <Navigate to="/login" />} 
-            />
-            <Route 
-              path="/workouts" 
-              element={user ? <Home /> : <Navigate to="/login" />} 
-            />
-            <Route 
-              path="/profile" 
-              element={user ? <UserProfile /> : <Navigate to="/login" />} 
+              element={user ? <Home />:<Navigate to="/login"/>} 
             />
             <Route 
               path="/login" 
-              element={!user ? <Login /> : <Navigate to="/" />} 
+              element={!user ? <Login />: <Navigate to="/"/>} 
             />
             <Route 
               path="/signup" 
-              element={!user ? <Signup /> : <Navigate to="/" />} 
+              element={!user ? <Signup />: <Navigate to="/"/>} 
             />
           </Routes>
         </div>
-        <Footer />
+        <Footer/>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
